a097c9e1d5ec1bb96bac1632945f4923e2178add
Im-grill <fakkukawaiiuwu@gmail.com>
2023-04-23 15:25:49 +0700

    Added a button in edit views

--- WEB301_GROUP-P/resources/views/customer/edit.blade.php
+++ WEB301_GROUP-P/resources/views/customer/edit.blade.php
@@ -30,5 +30,6 @@ background: radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 
         </div>
         <div>
             <button type="submit" class="btn btn-outline-success">Update</button>
+            <button type="cancel" class="btn btn-outline-secondary" a href="/index">Cancel</button>
         </div>
 @endsection
