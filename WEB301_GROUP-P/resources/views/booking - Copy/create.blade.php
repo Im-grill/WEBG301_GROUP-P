6535338680be88241e3357246c7b0f89b95776b2
Taihck <ngphuthai@gmail.com>
2023-04-25 03:51:52 +0700

    booking
    4 phần view

---
+++ WEB301_GROUP-P/resources/views/booking - Copy/create.blade.php
@@ -0,0 +1,80 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Create Booking</h1>
+    <form action="{{ route('bookings.store') }}" method="POST">
+        @csrf
+        <div>
+            <label for="customer_id">Customer</label>
+            <select name="customer_id" id="customer_id">
+                @foreach ($customers as $customer)
+                    <option value="{{ $customer->id }}">{{ $customer->name }}</option>
+                @endforeach
+            </select>
+        </div>
+        <div>
+            <label for="pitch_id">Pitch</label>
+            <select name="pitch_id" id="pitch_id">
+                @foreach ($pitches as $pitch)
+                    <option value="{{ $pitch->id }}">{{ $pitch->name }}</option>
+                @endforeach
+            </select>
+        </div>
+        <button type="submit">Create Booking</button>
+    </form>
+@endsection 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
